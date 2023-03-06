3ba5b9ec6b0e16aac76919bc116b70a35829606a
ChristopheBaj <108456770+ChristopheBaj@users.noreply.github.com>
2023-03-06 14:14:16 +0900

    Update streamlit_app.py

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -1103,6 +1103,29 @@ elif choice == 'Bayesian':
 				mime = 'text/csv')
 
 if choice == 'About':
+	st.header("Contact")
+
+	contact_form = """
+	<form action="https://formsubmit.co/{}" method="POST">
+	     <input type="hidden" name="_captcha" value="false">
+	     <input type="text" name="name" placeholder="Your name" required>
+	     <input type="email" name="email" placeholder="Your email" required>
+	     <textarea name="message" placeholder="Your message here"></textarea>
+	     <button type="submit">Send</button>
+	</form>
+	""".format(st.secrets["email_address"])
+
+	st.markdown(contact_form, unsafe_allow_html=True)
+
+	# Use Local CSS File
+	def local_css(file_name):
+	    path = os.path.dirname(__file__)
+	    file_name = path+"/"+file_name
+	    with open(file_name) as f:
+	        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+	local_css("style/email_style.css")
+####
 	def read_markdown_file(markdown_file):
 	    return Path(markdown_file).read_text()
 
