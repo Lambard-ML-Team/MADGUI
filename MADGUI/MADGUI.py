3eef3d469f81fd6356574c37303488e37c568243
Christophe BAJAN <108456770+ChristopheBaj@users.noreply.github.com>
2023-05-16 11:17:38 +0900

    Update MADGUI.py

--- MADGUI/MADGUI.py
+++ MADGUI/MADGUI.py
@@ -75,7 +75,8 @@ def convert_feat_lim(df):
 # The navigation between pages will be on the sidebar with the data uploader and the reset button
 
 with st.sidebar:
-	st.image('./madgui_logo.png',use_column_width='auto')
+	os.path.join(os.getcwd(), "./madgui_logo.png")
+	st.image('os.path.join(os.getcwd(), "./madgui_logo.png")',use_column_width='auto')
 	# Navigation part in the sidebar
 	choice = option_menu('Navigation', ['Main Page','Prediction','Bayesian','About','Contact'],
 		icons = ['house', 'tree','app-indicator','info-circle','mailbox'],
